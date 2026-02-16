6e0f7a11bbbf35dc4501c090e9ed5b56a7d744da
Dictator <dictatorbogdan@gmail.com>
2026-02-17 01:15:12 +0200

    add js-hw11 pixabay-api

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const API_KEY = '28315893-0fd806bb9dd4884845b8c425c';
+const url = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  if (!query.trim()) {
+    alert('Пустий рядок');
+    return;
+  }
+
+  console.log(query);
+  axios(url, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  })
+    .then(res => {
+      if (res.data.hits.length > 0) {
+        console.log(res.data.hits);
+      } else {
+        iziToast.show({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          color: 'red',
+          position: 'topRight',
+        });
+      }
+    })
+    .catch(err => console.log(err));
+}
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { getImagesByQuery } from './js/pixabay-api';
+// import renderFc from './js/render-functions';
+
+const form = document.querySelector('.form');
+
+console.log(form);
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(evt.target.elements);
+
+  const searchValue = evt.target.elements['search-text'].value;
+
+  getImagesByQuery(searchValue);
+}
